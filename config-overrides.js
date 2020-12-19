1130b5c2dfcc91d38da4bea8b67f27f32776a512
Dante Calderon <mail@dantecalderon.dev>
2020-12-19 13:19:52 -0500

    Fix taiwin configuration

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,12 @@
 const { override, addWebpackAlias, addPostcssPlugins } = require('customize-cra')
+const tailwindcss = require('tailwindcss')
+const { join } = require('path')
+
+const taiwinConfig = join(__dirname, './tailwind.config.js')
 
 module.exports = override(
   addWebpackAlias({ 'react-dom': '@hot-loader/react-dom' }),
-  addPostcssPlugins([ require('tailwindcss') ])
+  addPostcssPlugins([
+    tailwindcss(taiwinConfig)
+  ])
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
+/* eslint-disable no-restricted-syntax */
+const { join } = require('path')
+
+console.log('Processing el taiwins for', process.env.NODE_ENV)
+
+const contentFiles = join(__dirname, './src/**/*.tsx')
+
 module.exports = {
-  purge   : [],
-  darkMode: true, // or 'media' or 'class'
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      contentFiles
+    ]
+  },
+  darkMode: 'media', // or 'media' or 'class'
   theme   : {
     extend: {}
   },

455c466af145681ea2aae3edea1faba4a3f1ba1f
Dante Calderon <mail@dantecalderon.dev>
2021-01-03 19:54:56 -0500

    Add CI workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Root from './containers/Root';
+import createRoutes from './routes';
+import store, { history } from './store/configureStore';
 
-import store, { history } from './store/configureStore'
-import createRoutes from './routes'
-import Root from './containers/Root'
+const routes = createRoutes(history);
 
-const routes = createRoutes(history)
+const App: React.FC<Record<string, unknown>> = () => (
+  <Root store={store}>{routes}</Root>
+);
 
-const App:  React.FC<Record<string, unknown>> = () => <Root store={store}>{routes}</Root>
-
-export default App
+export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,8 @@
+import { render } from '@testing-library/react'
 import React from 'react'
-import { render, waitForElement } from '@testing-library/react'
 import App from '../App'
 
 it('renders correctly', async () => {
   const tree = render(<App />)
-  await waitForElement(() => tree.getByTestId('counter'))
   expect(tree).toMatchSnapshot()
 })
